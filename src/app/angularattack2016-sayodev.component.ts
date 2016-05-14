b04db7b83707aee8d4945c4bedcf9b014f451568
CNLambda <doris-books@dorimara.de>
2016-05-14 13:49:18 +0200

    Made the router work.

--- src/app/angularattack2016-sayodev.component.ts
+++ src/app/angularattack2016-sayodev.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HomeComponent } from './+home';
-import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouterLink} from '@angular/router';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
 import { CardCollectionComponent } from './card-collection';
 import { UiOverlayComponent } from './ui-overlay';
 import { ChatComponent } from './chat';
@@ -11,12 +11,18 @@ import { UsersComponent } from './users';
   selector: 'angularattack2016-sayodev-app',
   templateUrl: 'angularattack2016-sayodev.component.html',
   styleUrls: ['angularattack2016-sayodev.component.css'],
-  directives: [ROUTER_DIRECTIVES, RouterLink, CardCollectionComponent, UiOverlayComponent, ChatComponent, UsersComponent],
+  directives: [ROUTER_DIRECTIVES, CardCollectionComponent, UiOverlayComponent, ChatComponent, UsersComponent],
   providers: [ROUTER_PROVIDERS]
 })
 @Routes([
     {path: '/home', component: HomeComponent}
 ])
-export class Angularattack2016SayodevAppComponent {
-  title = 'angularattack2016-sayodev works!';
+export class Angularattack2016SayodevAppComponent implements OnInit {
+
+    constructor(private router: Router) {}
+
+    ngOnInit() {
+        this.router.navigate(['/home']);
+    }
+
 }
