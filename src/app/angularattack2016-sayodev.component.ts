0ccbec46ca8e28a61b19424f5c0bd5f7a5f3f060
angular-cli <angular-cli@angular.io>
2016-05-14 08:17:23 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/angularattack2016-sayodev.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'angularattack2016-sayodev-app',
+  templateUrl: 'angularattack2016-sayodev.component.html',
+  styleUrls: ['angularattack2016-sayodev.component.css']
+})
+export class Angularattack2016SayodevAppComponent {
+  title = 'angularattack2016-sayodev works!';
+}
---
+++ src/app/index.ts
@@ -0,0 +1,2 @@
+export {environment} from './environment';
+export {Angularattack2016SayodevAppComponent} from './angularattack2016-sayodev.component';
