97737ab08019897bf55901557bdbbb0071246e49
elias <elias@eliaslenovo>
2016-05-14 22:18:35 +0200

    session service init...

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SessionService {
-
-  constructor() {}
-
+  private storage_id = "aa_sayodev_storage";
+  private getRawObject(){
+    return localStorage.getItem(this.session_id);
+  }
+  private setRawObject(data){
+    localStorage.setItem(this.session_id, data);
+  }
+  constructor() {
+    if(getRawObject() == null){
+      setRawObject({});
+    }
+  }
 }
