25dd47745555bbea302165c7c94f5f9271ed60ff
elias <elias@eliaslenovo>
2016-05-14 08:52:08 +0200

    Now with build.

---
+++ dist/app/angularattack2016-sayodev.component.js
@@ -0,0 +1,28 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var Angularattack2016SayodevAppComponent = (function () {
+    function Angularattack2016SayodevAppComponent() {
+        this.title = 'angularattack2016-sayodev works!';
+    }
+    Angularattack2016SayodevAppComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'angularattack2016-sayodev-app',
+            templateUrl: 'angularattack2016-sayodev.component.html',
+            styleUrls: ['angularattack2016-sayodev.component.css']
+        }), 
+        __metadata('design:paramtypes', [])
+    ], Angularattack2016SayodevAppComponent);
+    return Angularattack2016SayodevAppComponent;
+}());
+exports.Angularattack2016SayodevAppComponent = Angularattack2016SayodevAppComponent;
+//# sourceMappingURL=angularattack2016-sayodev.component.js.map
---
+++ dist/app/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var environment_1 = require('./environment');
+exports.environment = environment_1.environment;
+var angularattack2016_sayodev_component_1 = require('./angularattack2016-sayodev.component');
+exports.Angularattack2016SayodevAppComponent = angularattack2016_sayodev_component_1.Angularattack2016SayodevAppComponent;
+//# sourceMappingURL=index.js.map
