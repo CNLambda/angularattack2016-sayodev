61a2ccdc5ec9d8573847f0a295d468ea615001e7
CNLambda <doris-books@dorimara.de>
2016-05-14 12:45:19 +0200

    Added imports.

--- src/app/angularattack2016-sayodev.component.ts
+++ src/app/angularattack2016-sayodev.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './+home';
 import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
+import { CardCollectionComponent } from './card-collection';
+import { UiOverlayComponent } from './ui-overlay';
+import { ChatComponent } from './chat';
+import { UsersComponent } from './users';
 
 @Component({
   moduleId: module.id,
--- src/app/card-collection/card-collection.component.ts
+++ src/app/card-collection/card-collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardComponent } from '../card';
 
 @Component({
   moduleId: module.id,
